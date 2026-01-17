8972372fe68ade0ffbccc821f6db3acf69a14dbc
iraizo <admin@raizo.dev>
2026-01-17 23:32:19 +0100

    nvm bruh

--- plugins/bypass-id/index.jsx
+++ plugins/bypass-id/index.jsx
@@ -1,5 +1,5 @@
 /** @type {import("@uwu/shelter-defs")} */
-export default {
+{
   onLoad: async () => {
     const {
       util: { log },
@@ -11,15 +11,21 @@ export default {
 
     log("ID Verification bypass loaded :3c");
 
-    patcher.after("getCurrentUser", UserStore.__proto__, (_, v) => {
-      if (v) v.ageVerificationStatus = 3;
+    patcher.after("getCurrentUser", UserStore, (_, v) => {
+      if (v) {
+        v.ageVerificationStatus = 3;
+      }
     });
 
-    patcher.instead("setRequestHeader", XMLHttpRequest.prototype, function(args, orig) {
-      if (args[0] == "X-Super-Properties") {
-        args[1] = "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImVuLUdCIiwiaGFzX2NsaWVudF9tb2RzIjpmYWxzZSwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NClBcHBsZVdlYktpdC81MzcuMzYgKEtIVE1MLCBsaWtlIEdlY2tvKSBDaHJvbWUvMTIwLjAuMC4wIFNhZmFyaS81MzcuMzYiLCJicm93c2VyX3ZlcnNpb24iOiIxMjAuMCIsIm9zX3ZlcnNpb24iOiIxMCIsInJlZmVycmVyIjoiIiwicmVmZXJyaW5nX2RvbWFpbiI6IiIsInJlZmVycmVy_..._";
+    patcher.instead("setRequestHeader", XMLHttpRequest.prototype, function (args, orig) {
+      if (args[0] === "X-Super-Properties") {
+        args[1] = "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImVuLUdCIiwiaGFzX2NsaWVudF9tb2RzIjpmYWxzZSwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NClBcHBsZVdlYktpdC81MzcuMzYgKEtIVE1MLCBsaWtlIEdlY2tvKSBDaHJvbWUvMTIwLjAuMC4wIFNhZmFyaS81MzcuMzYiLCJicm93c2VyX3ZlcnNpb24iOiIxMjAuMCIsIm9zX3ZlcnNpb24iOiIxMCIsInJlZmVycmVyIjoiIiwicmVmZXJyaW5nX2RvbWFpbiI6IiJ9";
       }
       return orig.apply(this, args);
     });
   },
+
+  onUnload: () => {
+    shelter.util.log("ID Verification bypass unloaded");
+  }
 }
